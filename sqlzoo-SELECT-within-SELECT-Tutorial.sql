676378fd1fdbb60982881fc82cd5306eeeedd2d8
Anton Schneider <56325076+antonschneider90@users.noreply.github.com>
2020-03-12 19:27:58 -0700

    Update sqlzoo-SELECT-within-SELECT-Tutorial.sql

--- sqlzoo-SELECT-within-SELECT-Tutorial.sql
+++ sqlzoo-SELECT-within-SELECT-Tutorial.sql
@@ -3,7 +3,7 @@
 SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
-      WHERE name='Russia')
+      WHERE name = 'Russia')
       
  
 /* 2 Show the countries in Europe with a per capita GDP greater than 'United Kingdom'. */
@@ -14,3 +14,55 @@ WHERE continent = 'Europe' AND gdp/population >
 (SELECT gdp/population
 FROM world
 WHERE name = 'United Kingdom')
+
+
+/* 3 List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country. */
+
+SELECT name, continent
+FROM world
+WHERE continent IN
+(SELECT continent
+FROM world
+WHERE name IN ('Argentina', 'Australia'))
+ORDER BY name
+               
+
+/* 4 Which country has a population that is more than Canada but less than Poland? Show the name and the population. */
+               
+SELECT name, population
+FROM world
+WHERE population >
+(SELECT population
+FROM world
+WHERE name = 'Canada')
+AND population <
+(SELECT population
+FROM world
+WHERE name = 'Poland')
+
+               
+/* 5 Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany. */
+
+SELECT name,
+CONCAT(
+ROUND((population/(SELECT population FROM world WHERE name = 'Germany')*100),0),
+'%') AS '% of GER pop'
+FROM world
+WHERE continent = 'Europe'
+               
+
+/* 6 Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values) */
+
+SELECT name
+FROM world
+WHERE gdp >
+(SELECT gdp
+FROM world
+WHERE continent = 'Europe'
+ORDER BY gdp DESC
+LIMIT 1)
+
+
+/* 7-10 to add */
